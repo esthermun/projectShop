2bf28681ce0868e8580d10cc5cb1fc2360d40d44
esthermun <esther_mun@hotmail.com>
2015-08-04 00:11:41 -0400

    set up navbar with sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # if current_user.admin?
+  # 	redirect_to admin page
+  # end
+  #if the page doesnt have current_user set then use this.
+	# if current_user.try(:admin?)
+	# 	flash[:alert] = "Access denied"
+	# 	redirect_to root_path
+	# end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -9,5 +9,5 @@ class Product < ActiveRecord::Base
 	validates :name, :description, :price, :quantity, :sku, presence: true
 	validates :name, length: {minimum: 2, maximum: 30}
 	validates :description, length: {minimum: 5, maximum: 300}
-	validates :price, :quantity, numericality: true
+	validates :price, :quantity, numericality: {greater_than_or_equal_to: 1}
 end
