36c8e7b016bf5775aabcf574433c6255ba9f316a
esthermun <esther_mun@hotmail.com>
2015-08-04 00:48:09 -0400

    added active admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  # if current_user.admin?
-  # 	redirect_to admin page
+  # def after_sign_in_path_for(resource)
+  #   if current_user.admin?
+  # 	 redirect_to admin_root_path
+  #   else
+  #     root_path
+  #   end
   # end
+  
   #if the page doesnt have current_user set then use this.
 	# if current_user.try(:admin?)
 	# 	flash[:alert] = "Access denied"
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  root to: 'home#index'
   # resources 'orders'
 
   resources 'categories'
 
   resources 'products'
 
-  root to: 'home#index'
+  ActiveAdmin.routes(self)
+
+  devise_for :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
